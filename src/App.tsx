41da5ff55a3ee1035ddb98a4dbb9894f021a5f45
pkh0106 <rudgml742@gmail.com>
2023-10-05 18:11:13 +0900

    Feat : 파일 생성 및 헤더 연결

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Main } from "./component/project/main";
 import { MyPage } from "./component/project/mypage";
 import { SignIn } from "./component/project/sign/signIn";
 import { SignUp } from "./component/project/sign/signUp";
+import { Notebook } from "./component/project/maternityNotebook/notebook";
+import { Diary } from "./component/project/maternityNotebook/diary";
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,8 @@ function App() {
         <Route path="/Mypage" element={<MyPage />} />
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Notebook" element={<Notebook />} />
+        <Route path="/Diary" element={<Diary />} />
       </Routes>
     </div>
   );
