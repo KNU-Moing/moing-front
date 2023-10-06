60b98906b20cef46bb199fa2dc7edfec5ed50d2a
pkh0106 <rudgml742@gmail.com>
2023-10-06 18:52:35 +0900

    Feat: 일기 페이지 구현 완료 (#11)

--- src/component/project/maternityNotebook/diary/component.tsx
+++ src/component/project/maternityNotebook/diary/component.tsx
@@ -1,7 +1,9 @@
 import theme from "../../../../styles/theme";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { RiArrowDropDownLine } from "react-icons/ri";
 import styled from "@emotion/styled";
+import { useState } from "react";
 
 export const TitleBar = ({ children }: { children: React.ReactNode }) => (
   <div
@@ -22,6 +24,8 @@ export const WriteButton = () => {
   return (
     <button
       css={css`
+        display: flex;
+        align-items: center;
         border: none;
         background-color: #ffffff;
         padding: 0 ${theme.spacing.md};
@@ -53,7 +57,12 @@ export const TitleContent = () => {
   );
 };
 
-export const SortSelect = ({
+/*
+*
+커스텀 select
+*
+*/
+export const CustomSelect = ({
   setState,
   selected,
   sortList,
@@ -62,20 +71,103 @@ export const SortSelect = ({
   selected: string;
   sortList: string[];
 }) => {
-  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setState(e.target.value);
+  const [isOpened, setIsOpened] = useState(false);
+  const handleClick = () => {
+    setIsOpened(!isOpened);
   };
   return (
-    <select onChange={handleSelect} value={selected}>
-      {sortList.map((item) => (
-        <option value={item} key={item}>
-          {item}
-        </option>
-      ))}
-    </select>
+    <div>
+      <SelectBtn onClick={handleClick}>
+        <RiArrowDropDownLine></RiArrowDropDownLine>
+        {selected}
+      </SelectBtn>
+      <SelectUl isShown={isOpened}>
+        {sortList.map((item) => (
+          <SelectLi value={item} setState={setState}></SelectLi>
+        ))}
+      </SelectUl>
+    </div>
+  );
+};
+const SelectBtn = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}) => (
+  <button
+    css={css`
+      width: 7rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border: none;
+      background-color: #ffffff;
+      color: ${theme.palette.gray[600]};
+      font-size: ${theme.typography.body4.fontSize};
+      font-weight: ${theme.typography.body4.fontWeight};
+    `}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+const SelectUl = ({
+  children,
+  isShown,
+}: {
+  children: React.ReactNode;
+  isShown: boolean;
+}) => (
+  <ul
+    css={css`
+      list-style-type: none;
+      padding: 0;
+      margin: 4px 0 0 0;
+      position: absolute;
+      cursor: pointer;
+      box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+      border-radius: 5px;
+      display: ${isShown ? "inline-block" : "none"};
+      background-color: #fff;
+      font-size: ${theme.typography.body4.fontSize};
+      font-weight: ${theme.typography.body4.fontWeight};
+    `}
+  >
+    {children}
+  </ul>
+);
+const SelectLi = ({
+  value,
+  setState,
+}: {
+  value: string;
+  setState: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const handleChoose = () => {
+    setState(value);
+  };
+  return (
+    <li
+      css={css`
+        padding: 0.2rem 0.5rem 0.2rem 2rem;
+        &:hover {
+          background-color: ${theme.palette.gray[200]};
+        }
+      `}
+      onClick={handleChoose}
+    >
+      {value}
+    </li>
   );
 };
 
+/*
+*
+일기 목록
+*
+*/
 export const DiaryOverlay = ({ children }: { children: React.ReactNode }) => (
   <div
     css={css`
@@ -84,6 +176,7 @@ export const DiaryOverlay = ({ children }: { children: React.ReactNode }) => (
       padding: ${theme.spacing.sm} 0;
       color: ${theme.palette.gray[600]};
       flex-wrap: wrap;
+      gap: ${theme.spacing.lg};
       ${theme.typography.body4Bold};
     `}
   >
@@ -91,6 +184,72 @@ export const DiaryOverlay = ({ children }: { children: React.ReactNode }) => (
   </div>
 );
 
+interface DiaryData {
+  index: number;
+  img: string;
+  week: string;
+  content: string;
+}
 export const DiaryBox = () => {
-  return;
+  const [diary, setDiary] = useState({
+    index: 1,
+    img: "/img/DummyPicture.png",
+    week: "14주차",
+    content:
+      "안녕 아가야 ~ 이제 우리 아가가 곧 나오겠구나 우리 쑥쑥이 빨리 보고싶다! 쑥쑥아 근데 밤에 왜이렇게 발로 차는거니 힘이 정말 장사야 엄마 넘 힘들어 좀 살살차봐 ~ 그리고 우리 쑥쑥이 지금 여름인데 수박을 왜이렇게 좋아하니!! 쑥쑥이는 수박이 정말 좋나봐 ㅠㅠ 어쩌구 저쩌구 많을 경우 자를 거임",
+  });
+
+  const handleNavi = () => {
+    alert(`diary/${diary.index}페이지로 이동`);
+  };
+  return (
+    <div
+      css={css`
+        padding: ${theme.spacing.md};
+        width: 36vw;
+        border-radius: 10px;
+        border: 1px solid ${theme.palette.gray[500]};
+      `}
+      onClick={handleNavi}
+    >
+      <DiaryImg src={diary.img}></DiaryImg>
+      <DiaryTitle week={diary.week}></DiaryTitle>
+      <DiaryContent content={diary.content}></DiaryContent>
+    </div>
+  );
+};
+const DiaryImg = ({ src }: { src: string }) => {
+  return (
+    <img
+      css={css`
+        width: 100%;
+      `}
+      src={src}
+    ></img>
+  );
+};
+const DiaryTitle = ({ week }: { week: string }) => {
+  return (
+    <div
+      css={css`
+        width: 100%;
+        margin: ${theme.spacing.xs} 0;
+      `}
+    >
+      {week}
+    </div>
+  );
+};
+const DiaryContent = ({ content }: { content: string }) => {
+  return (
+    <div
+      css={css`
+        width: 100%;
+        margin: ${theme.spacing.xs} 0 0 0;
+        ${theme.typography.body5};
+      `}
+    >
+      {content.length < 130 ? content : content.slice(0, 125) + "..."}
+    </div>
+  );
 };
