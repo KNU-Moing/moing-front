e172dad467159ffe18df2aaa679f8b425f661d7f
pkh0106 <rudgml742@gmail.com>
2023-10-06 21:24:15 +0900

    Feat : 오늘의 진료 내용 구현 시작(#11)

--- src/component/project/maternityNotebook/notebook/component.tsx
+++ src/component/project/maternityNotebook/notebook/component.tsx
@@ -1,1 +1,51 @@
-export {};
+import theme from "../../../../styles/theme";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useState } from "react";
+
+/*
+*
+오늘의 진료 기록
+*
+*/
+export const TodayChart = ({ children }: { children: React.ReactNode }) => (
+  <div
+    css={css`
+      color: ${theme.palette.gray[600]};
+      padding: ${theme.spacing.sm} 0;
+    `}
+  >
+    {children}
+  </div>
+);
+
+export const ImgContainer = ({ imgList }: { imgList: string[] }) => (
+  <div
+    css={css`
+      display: flex;
+      gap: ${theme.spacing.md};
+      padding: ${theme.spacing.sm} 0;
+    `}
+  >
+    {imgList.map((img) => (
+      <img
+        css={css`
+          width: 32vw;
+        `}
+        src={img}
+      ></img>
+    ))}
+  </div>
+);
+
+export const ChartDetailLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => <div css={css``}>{children}</div>;
+
+export const ChartDetailSection = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => <div css={css``}>{children}</div>;
--- src/component/project/maternityNotebook/notebook/index.tsx
+++ src/component/project/maternityNotebook/notebook/index.tsx
@@ -3,12 +3,25 @@ import {
   Category,
   CategoryButton,
   ContextOverlay,
+  TitleContent,
 } from "../component";
+import {
+  TodayChart,
+  ImgContainer,
+  ChartDetailLayout,
+  ChartDetailSection,
+} from "./component";
 import { useNavigate } from "react-router-dom";
 import Header from "../../header/header";
+import { useEffect, useState } from "react";
 
 export const Notebook = () => {
   const navigate = useNavigate();
+  const [imgList, setDiaryList] = useState([
+    "/img/DummyPicture.png",
+    "/img/DummyPicture.png",
+    "/img/DummyPicture.png",
+  ]);
 
   return (
     <div>
@@ -26,7 +39,15 @@ export const Notebook = () => {
             onClick={() => navigate("/diary")}
           ></CategoryButton>
         </Category>
-        <ContextOverlay>내용</ContextOverlay>
+        <ContextOverlay>
+          <TodayChart>
+            <TitleContent content="오늘의 진료 기록"></TitleContent>
+            <ImgContainer imgList={imgList}></ImgContainer>
+            <ChartDetailLayout>
+              <ChartDetailSection>오늘의 진료 기록 디테일</ChartDetailSection>
+            </ChartDetailLayout>
+          </TodayChart>
+        </ContextOverlay>
       </Overlay>
     </div>
   );
