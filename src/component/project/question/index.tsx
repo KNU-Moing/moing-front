9dafb62fd505874f2506f56d7dfc3eee7f15ced5
paicearea <01lily0302@naver.com>
2023-10-10 12:24:24 +0900

    header 주석 추가

--- src/component/project/question/index.tsx
+++ src/component/project/question/index.tsx
@@ -29,7 +29,7 @@ export const Question = () => {
     </StyledHeader>
       <ContainerDiv>
         <div>
-          <h2>오늘의 질문</h2>
+          <h2>이번주 질문</h2>
           <p>오늘은 날이 화창하네요 쑥쑥이 엄마가 오늘 낮에 먹고 싶었던 음식은 무엇인가요?</p>
         </div>
         <div>
