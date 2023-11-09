cc9d465fd9257a5d1bd02337e4494822c195b951
pkh0106 <rudgml742@gmail.com>
2023-11-09 21:55:16 +0900

    Feat: 푸터 구현 (#37)

--- src/component/project/Footer/component.tsx
+++ src/component/project/Footer/component.tsx
@@ -1,5 +1,92 @@
-const FooterComponent = () => {
-    return(<>컴포넌트 추가</>);
-}
+import theme from "../../../styles/theme";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-export default FooterComponent;
+export const Overlay = ({ children }: { children: React.ReactNode }) => (
+  <div
+    css={css`
+      border-top: 1px solid ${theme.palette.pink[100]};
+      padding: 1.4rem 7.5rem 1rem 7.5rem;
+      display: flex;
+      gap: ${theme.spacing.xs};
+    `}
+  >
+    {children}
+  </div>
+);
+
+export const Telephone = () => (
+  <div
+    css={css`
+      margin: 0.2rem 0.7rem 0.2rem 0.2rem;
+    `}
+  >
+    <Content
+      content="이용 약관"
+      styleColor={theme.palette.gray.black}
+      isBold={true}
+    ></Content>
+    <Content
+      content="고객센터 1234-1234 ( 평일 오전 9시 ~ 오후 6시 )"
+      styleColor={theme.palette.gray.black}
+      isBold={false}
+    ></Content>
+    <Content
+      content="의료진 고객센터 1234-1234 ( 평일 오전 9시 ~ 오후 6시 )"
+      styleColor={theme.palette.gray.black}
+      isBold={false}
+    ></Content>
+  </div>
+);
+
+export const PinkContainer = ({
+  topContent,
+  bottomContent,
+}: {
+  topContent: string;
+  bottomContent: string;
+}) => {
+  return (
+    <div
+      css={css`
+        margin: 1.4rem 3.2rem 0.2rem 1.4rem;
+      `}
+    >
+      <Content
+        content={topContent}
+        styleColor={theme.palette.pink[100]}
+        isBold={false}
+      ></Content>
+      <Content
+        content={bottomContent}
+        styleColor={theme.palette.pink[100]}
+        isBold={false}
+      ></Content>
+    </div>
+  );
+};
+
+const Content = ({
+  content,
+  styleColor,
+  isBold,
+}: {
+  content: string;
+  styleColor: string;
+  isBold: boolean;
+}) => {
+  return (
+    <div
+      css={css`
+        color: ${styleColor};
+        font-weight: ${isBold
+          ? theme.typography.body6Bold.fontWeight
+          : theme.typography.body6.fontWeight};
+        font-size: ${theme.typography.body6.fontSize};
+        padding: 0.18rem;
+      `}
+    >
+      {content}
+    </div>
+  );
+};
