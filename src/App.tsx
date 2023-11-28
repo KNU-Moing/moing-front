fab6bac1165a6cf0dc6bcc1c7e90142b95caf574
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-11-28 19:46:54 +0900

    Feat - 조리원 등록

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { Supplies } from "./component/project/supplies";
 import { Question } from "./component/project/question/questionWrite";
 import QuestionConfirm from "./component/project/question/questionConfirm";
 import { Detail2 } from "./component/project/supplies/detail.tsx";
+import { RegistCook } from "./component/project/cook/regist.tsx";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
         <Route path="/Diary" element={<Diary />} />
         <Route path="/Cook" element={<Cook />} />
         <Route path="/Cook/:item" element={<Detail />} />
+        <Route path="/CookRegist/:item" element={<RegistCook />} />
         <Route path="/Supplies" element={<Supplies />} />
         <Route path="/Supplies/:item" element={<Detail2 />} />
         <Route path="/Community" element={<Community />} />
